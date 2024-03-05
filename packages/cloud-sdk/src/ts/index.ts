e05f20b3d680ade366730b8e39f9e9723c5e9e13
fgsreally <1325041831@qq.com>
2024-03-05 18:45:39 +0800

    feature(cloud): add handlePackageTypings from laf to generate ts info

---
+++ packages/cloud-sdk/src/ts/index.ts
@@ -0,0 +1,28 @@
+// fork from https://github.com/labring/laf
+import { resolve } from 'path'
+import { NodePackageDeclarations, PackageDeclaration } from './declartion'
+
+export async function handlePackageTypings(packageName: string, nodeModulesRoot = resolve(process.cwd(), 'node_modules')) {
+  if (packageName === '@types/node') {
+    const pkr = new NodePackageDeclarations(nodeModulesRoot)
+    const rets = []
+    for (const name of NodePackageDeclarations.NODE_PACKAGES) {
+      const r = await pkr.getNodeBuiltinPackage(name)
+      rets.push(r)
+    }
+
+    return rets
+  }
+
+  // Gets a node built-in package types
+  if (NodePackageDeclarations.NODE_PACKAGES.includes(packageName)) {
+    const pkr = new NodePackageDeclarations(nodeModulesRoot)
+    const r = await pkr.getNodeBuiltinPackage(packageName)
+
+    return [r]
+  }
+
+  const pkd = new PackageDeclaration(packageName, nodeModulesRoot)
+  await pkd.load()
+  return pkd.declarations
+}
