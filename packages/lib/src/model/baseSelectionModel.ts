fbe6160192c99fcf224c2cc0e47cc142fc6824e8
fgsreally <1325041831@qq.com>
2024-07-18 17:28:57 +0800

    feature(lib): SelectionModel support scope

--- packages/lib/src/model/baseSelectionModel.ts
+++ packages/lib/src/model/baseSelectionModel.ts
@@ -1,5 +1,5 @@
 import { Global, Tag } from 'phecda-core'
-import type { VirtualNode } from '../core'
+import type { Scope, VirtualNode } from '../core'
 
 @Global
 @Tag('selection')
@@ -7,7 +7,7 @@ export class BaseSelectionModel<T extends Record<string, any>> {
   selectedNode: VirtualNode<T> | undefined
   hoverNode: VirtualNode<T> | undefined
   selectedPage: VirtualNode<T>
-
+  selectedScope: Scope
   selectNode(node: VirtualNode<T>) {
     this.selectedNode = node
   }
@@ -42,5 +42,31 @@ export class BaseSelectionModel<T extends Record<string, any>> {
 
   isActiveNode(node: VirtualNode<T>) {
     return this.activeNodeSet.has(node)
+  }
+
+  protected readonly scopeSet = new Set<Scope>()
+
+  get activeScopes() {
+    return [...this.scopeSet]
+  }
+
+  clearActiveScopes() {
+    this.activeNodeSet.clear()
+  }
+
+  deactiveScope(scope: Scope) {
+    this.scopeSet.delete(scope)
+  }
+
+  activeScope(scope: Scope) {
+    this.scopeSet.add(scope)
+  }
+
+  toggleScope(scope: Scope) {
+    this.isActiveScope(scope) ? this.deactiveScope(scope) : this.activeScope(scope)
+  }
+
+  isActiveScope(scope: Scope) {
+    return this.scopeSet.has(scope)
   }
 }
